2309957514cda9f4410f7e99ff01504c3deb71d0
joaordaniel <joaordaniel@gmail.com>
2022-01-11 18:40:52 +0000

    20210111 1840

--- script2.R
+++ script2.R
@@ -4,8 +4,10 @@ options(max.print = 10000)
 
 filenames <- c("https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_01_circulo_eleitoral_aveiro.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_02_circulo_eleitoral_beja.pdf",
+"https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_03_circulo_eleitoral_braga.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_04_circulo_eleitoral_braganca.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_05_circulo_eleitoral_castelo_branco.pdf",
+"https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_06_circulo_eleitoral_coimbra.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_07_circulo_eleitoral_evora.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_08_circulo_eleitoral_faro.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_09_circulo_eleitoral_guarda.pdf",
@@ -14,6 +16,7 @@ filenames <- c("https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/lista
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_13_circulo_eleitoral_porto.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_14_circulo_eleitoral_santarem.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_15_circulo_eleitoral_setubal.pdf",
+"https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_16_circulo_eleitoral_viana_castelo.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_17_circulo_eleitoral_vila_real.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_18_circulo_eleitoral_viseu.pdf",
 "https://www.cne.pt/sites/default/files/dl/eleicoes/2022_ar/listas_candidatos/2022ar_listas_candidatos_30_circulo_eleitoral_ra_madeira.pdf",
@@ -24,7 +27,12 @@ extract_data <- function(x){
   temp_circ <- sub(".*circulo_eleitoral_", "", x)
   temp <- stringr::str_split(temp, "\n")
   temp <- data.frame(mine = unlist(temp))
-  temp$partido <- grepl("^[A-Z]+", temp$mine) & (grepl("–", temp$mine) | grepl("-", temp$mine)) & !grepl("Miguel Almeida Corte-Real Gomes", temp$mine)
+  temp$partido <- grepl("^[A-Z]+", temp$mine) & (grepl("–", temp$mine) | grepl("-", temp$mine)) &
+    !grepl("Miguel Almeida Corte-Real Gomes", temp$mine) &
+    !grepl("Abel Filipe Marques Pereira – Independente", temp$mine) &
+    !grepl("Cristina Paula Silva Oliveira – Independente", temp$mine) &
+    !grepl("Marta Raquel dos Santos Macedo – Independente", temp$mine)
+    
   temp <- temp[temp$mine != "", ]
   temp <- temp[-(1:3), ]
   temp$candidatos_e <- grepl("Efetivos", temp$mine)
@@ -69,14 +77,15 @@ extract_data <- function(x){
 data1 <- lapply(filenames, extract_data)
 data1 <- do.call(rbind.data.frame, data1)
 
+#write.csv(unique(data1$Partido), "check_partidos.csv")
 partidos <- read.csv("partidos.csv", header = T, sep = ";")
 
 data1 <- data1 %>%
   mutate(Círculo = stringi::stri_replace_all_fixed(Círculo,
                                                    unique(data1$Círculo),
-                                                   c("Aveiro", "Beja", "Bragança", "Castelo Branco",
+                                                   c("Aveiro", "Beja", "Braga", "Bragança", "Castelo Branco", "Coimbra",
                                                      "Évora", "Faro", "Guarda", "Leiria", "Portalegre",
-                                                     "Porto", "Santarém", "Setúbal", "Vila Real", "Viseu",
+                                                     "Porto", "Santarém", "Setúbal", "Viana do Castelo", "Vila Real", "Viseu",
                                                      "Madeira", "Açores"), vectorize_all = FALSE),
          Partido = stringi::stri_replace_all_fixed(Partido,
                                                    partidos$original,
